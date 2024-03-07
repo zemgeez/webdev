4553ec1cc156e0ef728597772944e4f269156434
msf1console <avinavp829@gmail.com>
2024-03-07 10:25:29 +0530

    Initial Commit

---
+++ app/about/page.tsx
@@ -0,0 +1,46 @@
+'use client'
+import { title } from "@/components/primitives";
+import {Image} from "@nextui-org/react";
+import { Card, CardHeader,  CardFooter } from "@nextui-org/card";
+import { image } from "@nextui-org/theme";
+
+export default function AboutPage() {
+	return (
+
+		
+			<div>
+			
+			<Card className="bg-transparent bg-slate-600 shadow-lg">
+				<Image
+
+					
+      isZoomed
+      width={240}
+      alt="NextUI Fruit Image with Zoom"
+      src="https://nextui-docs-v2.vercel.app/images/fruit-1.jpeg"
+    />
+				<CardHeader>
+					 
+					<h1 className={title()}>
+					Header
+				</h1></CardHeader>
+				
+					<CardFooter >
+						<h5 className={title({size:"sm"})}>
+						</h5>Footer</CardFooter>
+			</Card>
+			
+			
+		</div>
+		
+	);
+}
+
+
+
+
+
+		
+
+
+
---
+++ app/page.tsx
@@ -0,0 +1,25 @@
+
+import { fontSans } from "@/config/fonts";
+import { Card, CardHeader,  CardFooter } from "@nextui-org/card";
+export default function home() {
+	return (
+		<div>
+			
+			<Card isFooterBlurred>
+				<CardHeader >
+					 
+					<h1>
+					Header
+				</h1></CardHeader>
+				
+				<CardFooter >Footer</CardFooter>
+			</Card>
+			
+			
+		</div>
+	);
+}
+		
+
+
+
