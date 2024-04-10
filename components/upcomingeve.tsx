59d3088902d6fab3230e3f83d35f84ebfdaad778
Harshita Kumari <kumariharshit025@gmail.com>
2024-04-10 16:45:29 +0530

    add upcoming events

--- components/upcomingeve.tsx
+++ components/upcomingeve.tsx
@@ -9,7 +9,7 @@ export default function UpcomingEvents() {
     <div className="grid grid-cols-2 py-10">
       <div className="grid grid-cols-1 px-4">
          <Card className="max-w-1/3 bg-transparent justify-center pb-4 px-4  ">
-          <h1 className='text-3xl text-white font-sans px-2  bg-transparent w-1/2'>Upcoming Events</h1>
+          <h1 className='text-3xl text-black font-sans px-2  bg-transparent w-1/2'>Upcoming Events</h1>
           <Carousel autoPlay showThumbs={false}   interval={3000} infiniteLoop className=" mt-1 w-full py-1 px-1 rounded-3xl  shrink-0">
             <div className="rounded-xl">
               <img src="./slide2.jpeg"  className ="aspect-video" alt="image1" />
@@ -28,7 +28,7 @@ export default function UpcomingEvents() {
       <div className="w-full px-4">
        
         <div className="grid grid-rows-2">
-          <div className="text-white text-center mt-auto text-3xl h-1/3 bg-slate-400 rounded-lg">Seminar</div>
+          <div className="text-white text-center mt-12 text-3xl h-1/3 bg-slate-400 rounded-lg">Seminar</div>
           <Carousel autoPlay showThumbs={false} interval={3000} infiniteLoop  axis="vertical" className=" mt-1 h-full w-full py-1 px-1 rounded-3xl  shrink-0 ">
           <div className="text-sm text-white overflow-y-auto px-4 rounded-lg max-h-40 scrollbar-hide justify-items-center justify-center bg-green-400">
             <p>Department of Computer Science and Engineering</p>
